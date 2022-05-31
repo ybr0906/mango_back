760fc578cdd80f10a2780d796b69f157e2f73acf
윤병록 <ybr@yunbyeonglog-ui-MacBookPro.local>
2022-05-31 13:43:19 +0900

    0531

--- src/utill/kakao.js
+++ src/utill/kakao.js
@@ -13,35 +13,46 @@ const alertTalk = (phone, address, category) => {
 
     if (category == 'store_as') {
         service = '매장방문';
-    } else if (category == 'business_trip_as') {
-        service = '출장방문';
-    } else if (category == 'as') {
-        service = '출장방문';
-    } else if (category == 'product') {
-        service = 'pc판매';
-    } else if (category == 'monitor') {
-        service = '노트북액정';
-    } else if (category == 'apple') {
-        service = '애플제품';
-    } else {
-        messageService.sendMany([
-            {
-                to: "01080094613",
-                from: "01090097747",
-                text: `서비스문의 ${phone} / ${address} / ${service}`
-            },
-            {
-                to: "01039596434",
-                from: "01090097747",
-                text: `서비스문의 ${phone} / ${address} / ${service}`
-            },
-            {
-                to: "01090097747",
-                from: "01090097747",
-                text: `서비스문의 ${phone} / ${address} / ${service}`
-            },
-        ]).then(res => console.log(res));
     }
+    if (category == 'business_trip_as') {
+        service = '출장방문';
+    }
+    if (category == 'as') {
+        service = '출장방문';
+    }
+    if (category == 'product') {
+        service = 'pc판매';
+    }
+    if (category == 'monitor') {
+        service = '노트북액정';
+    }
+    if (category == 'apple') {
+        service = '애플제품';
+    }
+
+    messageService.sendMany([
+        // {
+        //     to: "01080094613",
+        //     from: "01090097747",
+        //     text: `서비스문의 ${phone} / ${address} / ${service}`
+        // },
+        // {
+        //     to: "01039596434",
+        //     from: "01090097747",
+        //     text: `서비스문의 ${phone} / ${address} / ${service}`
+        // },
+        // {
+        //     to: "01090097747",
+        //     from: "01090097747",
+        //     text: `서비스문의 ${phone} / ${address} / ${service}`
+        // },
+        {
+            to: "01055322155",
+            from: "01090097747",
+            text: `서비스문의 ${phone} / ${address} / ${service}`
+        },
+    ]).then(res => console.log(res));
+
 }
 
 module.exports = alertTalk;
